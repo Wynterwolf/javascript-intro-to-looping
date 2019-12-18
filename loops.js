2f6bc6a1c8b735b29314a2fb535c9ea4ec014e37
Samantha Jordan <shinyguns@gmail.com>
2019-12-18 13:55:18 -0600

    Update loops.js
    first run

---
+++ loops.js
@@ -0,0 +1,27 @@
+// for loop with argument, start 0, 25 times add string to array 
+
+function forLoop(array) { 
+  for (var i = 0; i < 25; i++) {
+    console.log(`I am ${i} strange loop${i === 0 ? '' : 's'}.`) ;
+    }}
+    
+    
+// while loop 
+    
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  } if ( n === 0) {
+    console.log("done");
+  }
+}
+
+//  loop 
+
+function doWhileLoop(num) {
+  console.log("I run once regardless.");
+    } while ([num < 0]) {
+      console.log("")
+    }
+  
+//
