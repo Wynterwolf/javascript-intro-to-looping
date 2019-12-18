0b45c06698af6bccb1c27677e772fdac76b04fb0
Samantha Jordan <shinyguns@gmail.com>
2019-12-18 16:18:04 -0600

    Update loops.js
    1 pass

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-// for loop with argument, start 0, 25 times add string to array 
+/* TODO: Build a function forLoop. It takes an array as an argument. Start counting from 0, and, using a for loop, add a string to the array 25 times. But not just any string. If your i value is 1, add the string "I am 1 strange loop."; if your i value is anything else, add the string "I am ${i} strange loops.". (Remember flow control with if and else? And how do we interpolate i?) Then return the array. */
 
 function forLoop(array) { 
   for (var i = 0; i < 25; i++) {
@@ -6,22 +6,24 @@ function forLoop(array) {
     }}
     
     
-// while loop 
+/* Create a function called whileLoop in loops.js. The function should take a number as an argument. Using a while loop, count down (using console.log) from the passed in number to 0. Then return the string 'done' */
     
 function whileLoop(n) {
   while (n > 0) {
     console.log(--n);
-  } if ( n === 0) {
+  if (n === 0) {
     console.log("done");
-  }
+  }}}
+  
+  /* TODO: Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked. */
+  
+  function doWhileLoop(i) {
+    function incrementVariable() {
+  i = i + 1;
+  return i;
 }
 
-//  loop 
-
-function doWhileLoop(num) {
-  console.log("I run once regardless.");
-    } while ([num < 0]) {
-      console.log("")
-    }
-  
-//
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() > i);
+  }
