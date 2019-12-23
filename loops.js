85f3f7a6c9c74e93573c51c07b3940c9815d6892
Samantha Jordan <shinyguns@gmail.com>
2019-12-23 16:59:11 -0600

    Update loops.js
    remove parenths on done

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n) {
     console.log(--n);
   if (n === 0) {
   }}
-return("done")
+return "done"
 }
   
   /* TODO: Define a function called doWhileLoop in loops.js. The function should take an integer as an argument. Use the incrementVariable() function (you can copy it from this README) in the condition, and console log "I run once regardless." while incrementVariable() returns a number less than the parameter received. (Your condition might look something like incrementVariable() < num.) Remember that it should also console log when receiving 0 as a parameter because the do-while runs before the condition is checked. */
